e8386f20dbfbb1f9d76ea2bde555bfd9bbe01baf
Davis Creemer <davis@creemer.net>
2023-11-16 20:18:04 +0000

    Created variablized version for Terraform

---
+++ terraform/take3/variables.tf
@@ -0,0 +1,8 @@
+variable "hostname" { type = string }
+variable "vrf" { type = string }
+variable "net1_vlan" { type = string }
+variable "net1_name" { type = string }
+variable "net2_vlan" { type = string }
+variable "net2_name" { type = string }
+variable "net3_vlan" { type = string }
+variable "net3_name" { type = string }
